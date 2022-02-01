c8047c9698c815ab52eb7ab699517cabdf67603c
cleilsoncgb <cleilsoncgb@gmail.com>
2022-01-31 22:20:20 -0300

    Regras

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.di.desafio.dominio.Bootcamp;
 import br.com.di.desafio.dominio.Curso;
+import br.com.di.desafio.dominio.Dev;
 import br.com.di.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,8 +26,38 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCGB = new Dev();
+        devCGB.setNome("Cleilson");
+        devCGB.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Cleilson: "+devCGB.getConteudosInscritos());
+        devCGB.progredir();
+        System.out.println("Conteudos inscritos Cleilson: "+devCGB.getConteudosInscritos());
+        System.out.println("Conteudos inscritos Cleilson: "+devCGB.getConteudosConcluidos());
+        System.out.println("XP: "+devCGB.calcularTotalXp());
+
+        System.out.println("=========  ### ========");
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Maria: "+devMaria.getConteudosInscritos());
+        devMaria.progredir();
+        System.out.println("Conteudos inscritos Maria: "+devMaria.getConteudosInscritos());
+        System.out.println("Conteudos inscritos Maria: "+devMaria.getConteudosConcluidos());
+        System.out.println("XP: "+devMaria.calcularTotalXp());
+
+
+
     }
 }
